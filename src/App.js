005ee260103ec89991ea69f2a7ff11adeb296bb3
Natalia Reyes <aylin.reyes.8@gmail.com>
2025-04-10 12:01:05 -0500

    feat: add chromatic config

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-8">
+      <div className="max-w-4xl mx-auto">
+  test
+        </div>
+    </div>
+  );
+}
+
+export default App; 
